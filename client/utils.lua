f605f6c76f1a2d2a89a1e01870d75e55f679465b
Obtaizen <joshuamills9999@gmail.com>
2025-05-09 18:40:56 +0100

    [NoPixel Inspired]
    Added the nopixel inspired stuff

--- client/utils.lua
+++ client/utils.lua
@@ -29,7 +29,7 @@ function utils.raycastFromCamera(flag)
 end
 
 function utils.getTexture()
-    return lib.requestStreamedTextureDict('shared'), 'emptydot_32'
+    return lib.requestStreamedTextureDict('obtaizen'), 'Point-Green'
 end
 
 -- SetDrawOrigin is limited to 32 calls per frame. Set as 0 to disable.
@@ -37,8 +37,8 @@ local drawZoneSprites = GetConvarInt('ox_target:drawSprite', 24)
 local SetDrawOrigin = SetDrawOrigin
 local DrawSprite = DrawSprite
 local ClearDrawOrigin = ClearDrawOrigin
-local colour = vector(155, 155, 155, 175)
-local hover = vector(98, 135, 236, 255)
+local colour = 'Point-Green'
+local hover = 'Selected'
 local currentZones = {}
 local previousZones = {}
 local drawZones = {}
@@ -108,12 +108,11 @@ function utils.drawZoneSprites(dict, texture)
 
     for i = 1, drawN do
         local zone = drawZones[i]
-        local spriteColour = zone.colour or colour
+        local texture = zone.colour and hover or texture
 
-        if zone.drawSprite ~= false then
+        if zone.texture ~= false then
             SetDrawOrigin(zone.coords.x, zone.coords.y, zone.coords.z)
-            DrawSprite(dict, texture, 0, 0, width, height, 0, spriteColour.r, spriteColour.g, spriteColour.b,
-                spriteColour.a)
+            DrawSprite(dict, texture, 0, 0, width, height, 0, 255, 255, 255, 255, 255)
         end
     end
 
